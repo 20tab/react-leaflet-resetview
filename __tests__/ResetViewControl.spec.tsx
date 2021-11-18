9e3965323407275075daa9e1c99af3d9aa028d8c
valentinogagliardi <valentino@valentinog.com>
2021-11-18 14:32:04 +0100

    Configure coverage; bump version

--- __tests__/ResetViewControl.spec.tsx
+++ __tests__/ResetViewControl.spec.tsx
@@ -1,27 +1,32 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom";
 
 import { MapContainer, useMapEvents } from "react-leaflet";
 import ResetViewControl from "../src/ResetViewControl";
+import { ResetViewControlOptions } from "../dist/types/ResetViewControl";
 
 describe("ResetViewControl", () => {
   const mockHandleViewReset = jest.fn();
 
-  const ControlWrapper = () => {
+  const ControlWrapper = ({ title, icon }: ResetViewControlOptions) => {
     useMapEvents({
       viewreset: mockHandleViewReset,
     });
 
     return (
       <>
-        <ResetViewControl title="Reset view" />
+        <ResetViewControl
+          title={title ?? "Reset view"}
+          icon={icon ?? "\u2612"}
+        />
       </>
     );
   };
-  const Map = () => {
+  const Map = ({ title, icon }: ResetViewControlOptions) => {
     return (
       <MapContainer zoom={5} center={[-96.8716348, 32.8205866]}>
-        <ControlWrapper />
+        <ControlWrapper title={title} icon={icon} />
       </MapContainer>
     );
   };
@@ -33,5 +38,19 @@ describe("ResetViewControl", () => {
     userEvent.click(screen.getByTitle("Reset view"));
 
     expect(mockHandleViewReset).toHaveBeenCalledTimes(2);
+  });
+
+  test("can see icon", () => {
+    render(<Map />);
+
+    screen.getByText("\u2612");
+  });
+
+  test("can set icon", () => {
+    render(<Map icon="url(/some/relative/path.png)" />);
+
+    expect(screen.getByTitle("Reset view")).toHaveStyle({
+      "background-image": "url(/some/relative/path.png)",
+    });
   });
 });
